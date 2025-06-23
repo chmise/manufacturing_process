2fa8a5f691084e04c38e76d3dd42a6102e7d4cda
Blooming2081 <jtrobin35@gmail.com>
2025-06-23 15:45:50 +0900

    fix: file name

--- dashboard_frontend/src/components/Factory2D.jsx
+++ dashboard_frontend/src/components/Factory2D.jsx
@@ -1,5 +1,5 @@
 import Factory2DTwin from "./Twin/Factory2DTwin";
-import Navbar from "./Twin/navabr";
+import Navbar from "./Twin/Navabr";
 import ProductionTarget from './KPI/ProductionTarget';
 import HourlyProduction from './KPI/HourlyProduction';
 import CycleTime from './KPI/CycleTime';
@@ -14,19 +14,19 @@ const Factory2D = () => {
           <div className="card-body" style={{ height: "50rem" }}>
             <Navbar />
             {/* 왼쪽 생산 목표 카드 */}
-          <div className="card-body">
-            <div className="row g-3">
-              <div className="col-sm-12">
-                <ProductionTarget />
-              </div>
-              <div className="col-6">
-                <HourlyProduction />
-              </div>
-              <div className="col-6">
-                <CycleTime />
+            <div className="card-body">
+              <div className="row g-3">
+                <div className="col-sm-12">
+                  <ProductionTarget />
+                </div>
+                <div className="col-6">
+                  <HourlyProduction />
+                </div>
+                <div className="col-6">
+                  <CycleTime />
+                </div>
               </div>
             </div>
-          </div>
           </div>
         </div>
       </div>
