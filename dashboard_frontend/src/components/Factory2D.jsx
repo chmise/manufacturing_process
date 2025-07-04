025ec482264bdb9a49a3589dc3e98ce0096446d1
Blooming2081 <jtrobin35@gmail.com>
2025-07-04 17:21:15 +0900

    2d twin 수정

--- dashboard_frontend/src/components/Factory2D.jsx
+++ dashboard_frontend/src/components/Factory2D.jsx
@@ -1,18 +1,23 @@
+import React, { useState } from 'react';
 import Factory2DTwin from "./Twin/Factory2DTwin";
 import Navbar from "./Twin/Navabr";
 
-
-
 const Factory2D = () => {
+  // 선택된 제품 상태를 부모 컴포넌트에서 관리
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  // 제품 선택 핸들러
+  const handleProductSelect = (product) => {
+    setSelectedProduct(product);
+  };
 
   return (
     <div className="row g-3">
       <div className="col-sm-6 col-lg-4">
         <div className="card">
           <div className="card-body" style={{ height: "50rem" }}>
-            <Navbar />
-            {/* 왼쪽 생산 목표 카드 */}
-            
+            {/* Navbar에 선택된 제품 정보 전달 */}
+            <Navbar selectedProduct={selectedProduct} />
           </div>
         </div>
       </div>
@@ -20,7 +25,8 @@ const Factory2D = () => {
       <div className="col-sm-6 col-lg-8">
         <div className="card">
           <div className="card-body" style={{ height: "50rem" }}>
-            <Factory2DTwin />
+            {/* Factory2DTwin에 제품 선택 핸들러 전달 */}
+            <Factory2DTwin onProductSelect={handleProductSelect} />
           </div>
         </div>
       </div>
