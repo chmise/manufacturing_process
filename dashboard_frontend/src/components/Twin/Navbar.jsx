393ab46f8e606b69fab71c7e85818ed956895ce5
chmise <mnnnd4@gmail.com>
2025-07-29 00:50:17 +0900

    트윈 네비바 간격 수정

--- dashboard_frontend/src/components/Twin/Navbar.jsx
+++ dashboard_frontend/src/components/Twin/Navbar.jsx
@@ -7,23 +7,20 @@ import InternalEnvironment from '../Environment/InternalEnvironment';
 
 const Navbar = ({ selectedProduct }) => {  
   return (
-    <div className="d-flex flex-column h-100">
-      {/* 환경 카드들 - 더 작은 높이 */}
-      <div style={{ flex: '0.8 1 0', marginBottom: '0.8rem' }}>
+    <div className="d-flex flex-column h-100" style={{ gap: '0.8rem' }}>
+      <div style={{ height: '215px' }}>
         <ExternalEnvironment />
       </div>
       
-      <div style={{ flex: '0.8 1 0', marginBottom: '0.8rem' }}>
+      <div style={{ height: '215px' }}>
         <InternalEnvironment />
       </div>
       
-      {/* 생산 목표 - 일반 높이 */}
-      <div style={{ flex: '0.8 1 0'}}>
+      <div style={{ height: '215px' }}>
         <ProductionTarget />
       </div>
       
-      {/* 하단 시간당 생산수 + 사이클 타임 - 디지털 트윈 하단에 맞춤 */}
-      <div style={{ flex: '0.5 1 0' }}>
+      <div style={{ height: '215px' }}>
         <div className="d-flex h-100" style={{ gap: '0.5rem' }}>
           <div className="w-50">
             <HourlyProduction />
